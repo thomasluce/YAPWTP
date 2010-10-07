8341aca72b14af5ca41a4ce8abbea22bcc6b8afb
ThomasLuce <thomas@aboutus.org>
2010-10-07 12:42:29 -0700

    Now with Templatesgit add ffi/yapwtp.rb src/syntax.leg

--- ffi/yapwtp.rb
+++ ffi/yapwtp.rb
@@ -3,6 +3,19 @@
 require 'rubygems'
 require 'ffi'
 
+class Bstring < FFI::Struct
+  layout :data, :pointer,
+         :slen, :int,
+         :mlen, :int
+end
+
+class Node < FFI::Struct
+  layout :name, :pointer,
+         :content, :pointer,
+         :level, :int,
+         :next, :pointer # This is a pointer to a Node... Don't know how to do that yet...
+end
+
 module YAPWTP
   extend FFI::Library
   ffi_lib "libyapwtp.so" # Must be in your LD_LIBRARY_PATH
@@ -24,24 +37,44 @@ module YAPWTP
   attach_function :str_get_contents, [:string], :void
   # void set_base_url(char *str)
   attach_function :set_base_url, [:string], :void
+  # struct node *get_next_template(void)
+  attach_function :get_next_template, [], :pointer
+  # int get_template_count(void)
+  attach_function :get_template_count, [], :int
+
+  # char * bstr2cstr (const_bstring s, char z)
+  attach_function :bstr2cstr, [:pointer, :char], :string
+
+  # int bcstrfree (char * s)
+  attach_function :bcstrfree, [:string], :int
+
+  def self.next_template
+    template = Node.new(YAPWTP.get_next_template())
+    name = YAPWTP.bstr2cstr(template[:name], 20)
+    n = name.to_s
+    YAPWTP.bcstrfree(name)
+    return n
+  end
 end
 
 if __FILE__ == $0
   #YAPWTP.stdin_get_contents(YAPWTP.get_input_buffer)
   #YAPWTP.file_get_contents(YAPWTP.get_input_buffer, "../spec/fixtures/cnn.com")
   
-  200.times do |i|
+  #200.times do |i|
 
     # Example using Ruby to read a file.  Using the above C-implemented methods is barely faster.
     File.open("../spec/fixtures/cnn.com", "rb") do |f|
       YAPWTP.init
       YAPWTP.str_get_contents f.read
       YAPWTP.parse
+      #puts YAPWTP.get_template_count
+      puts YAPWTP.next_template
       #puts YAPWTP.get_output_buffer_cstr 
-      puts i
+      # puts i
       YAPWTP.cleanup
     end
 
-  end
+  #end
 
 end
