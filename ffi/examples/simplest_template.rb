73ddcca60124ae8b7edefcae8ca183c5519e67f5
Relistan <karl@matthias.org>
2010-10-23 20:44:20 -0700

    * Make template substitution work * Move exmaple code into its own folder * Example on the simplest case of template substitution

---
+++ ffi/examples/simplest_template.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+# --------------------------------------------------------------------------------
+# This example handles the simplest case of templates one layer deep
+# * Templates here ignore arguments
+# --------------------------------------------------------------------------------
+
+base_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+require "#{base_path}/yapwtp"
+
+parser = WikiParser.new
+templates = nil
+wikitext = nil
+
+# Open the requested file, parse, capture text and templates
+wikitext = parser.html_from_file("#{base_path}/../spec/fixtures/cnn.com")
+templates = parser.templates
+
+templates.each do |template|
+  template_file = File.join("templates", template[:name]) 
+  if File.exist? template_file
+    wikitext.gsub! /#{template[:replace_tag]}/, parser.html_from_file(template_file)
+  end
+end
+puts wikitext
